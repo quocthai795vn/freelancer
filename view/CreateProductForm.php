d244076f702394eb45e7ef89793d075900726edc
quocthai <thainq@smartosc.com>
2019-02-28 18:43:23 +0700

    update code

--- view/CreateProductForm.php
+++ view/CreateProductForm.php
@@ -1,6 +1,10 @@
 
 <!-- Special version of Bootstrap that only affects content wrapped in .bootstrap-iso -->
 <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
 
 <!-- Inline CSS based on choices in "Settings" tab -->
 <style>.bootstrap-iso .formden_header h2, .bootstrap-iso .formden_header p, .bootstrap-iso form{font-family: Arial, Helvetica, sans-serif; color: black}.bootstrap-iso form button, .bootstrap-iso form button:hover{color: white !important;} .asteriskField{color: red;}</style>
@@ -44,7 +48,39 @@
                         </label>
                         <input class="form-control" id="attribute-set" name="attribute-set" type="text"/>
                     </div>
-                    <div class="form-group">
+                    
+                    <div class="panel panel-default">
+  <div class="panel-body">
+  
+  <div id="education_fields">
+          
+        </div>
+       <div class="col-sm-5 nopadding">
+  <div class="form-group">
+    <input type="text" class="form-control" id="Schoolname" name="Schoolname[]" value="" placeholder="School name">
+  </div>
+</div>
+<div class="col-sm-5 nopadding">
+  <div class="form-group">
+    <input type="text" class="form-control" id="Major" name="Major[]" value="" placeholder="Major">
+  </div>
+</div>
+
+<div class="col-sm-2 nopadding">
+  <div class="form-group">
+    <div class="input-group">
+ 
+      <div class="input-group-btn">
+        <button class="btn btn-success" type="button"  onclick="education_fields();"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> </button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="clear"></div>
+  
+  </div>
+</div>
+<div class="form-group">
                         <div>
                             <button class="btn btn-primary " name="submit" type="submit">
                                 Submit
@@ -56,3 +92,41 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+
+var room = 1;
+function education_fields() {
+ 
+    room++;
+    var objTo = document.getElementById('education_fields')
+    var divtest = document.createElement("div");
+    divtest.setAttribute("class", "form-group removeclass"+room);
+    var rdiv = 'removeclass'+room;
+    divtest.innerHTML = `<div class="col-sm-5 nopadding">
+    <div class="form-group">
+        <input type="text" class="form-control" id="Schoolname" name="Schoolname[]" value="" placeholder="School name">
+    </div>
+</div>
+<div class="col-sm-5 nopadding">
+    <div class="form-group">
+        <input type="text" class="form-control" id="Major" name="Major[]" value="" placeholder="Major">
+    </div>
+</div>
+<div class="col-sm-2 nopadding">
+    <div class="form-group">
+        <div class="input-group">
+            <div class="input-group-btn">
+                <button class="btn btn-danger" type="button" onclick="remove_education_fields(${room});"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="clear"></div>`;
+    
+    objTo.appendChild(divtest)
+}
+   function remove_education_fields(rid) {
+       $('.removeclass'+rid).remove();
+   }
+</script>
