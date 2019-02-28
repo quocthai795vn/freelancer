f56d88837cd5aaa6bf091b7f0f3b4064874b3d08
quocthai <thainq@smartosc.com>
2019-02-28 18:47:59 +0700

    update form

--- view/CreateProductForm.php
+++ view/CreateProductForm.php
@@ -1,13 +1,27 @@
-
 <!-- Special version of Bootstrap that only affects content wrapped in .bootstrap-iso -->
-<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
+<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 
-
 <!-- Inline CSS based on choices in "Settings" tab -->
-<style>.bootstrap-iso .formden_header h2, .bootstrap-iso .formden_header p, .bootstrap-iso form{font-family: Arial, Helvetica, sans-serif; color: black}.bootstrap-iso form button, .bootstrap-iso form button:hover{color: white !important;} .asteriskField{color: red;}</style>
+<style>
+    .bootstrap-iso .formden_header h2,
+    .bootstrap-iso .formden_header p,
+    .bootstrap-iso form {
+        font-family: Arial, Helvetica, sans-serif;
+        color: black
+    }
+    
+    .bootstrap-iso form button,
+    .bootstrap-iso form button:hover {
+        color: white !important;
+    }
+    
+    .asteriskField {
+        color: red;
+    }
+</style>
 
 <!-- HTML Form (wrapped in a .bootstrap-iso div) -->
 <div class="bootstrap-iso">
@@ -19,7 +33,7 @@
                         <label class="control-label " for="sku">
                             SKU
                         </label>
-                        <input class="form-control" id="sku" name="sku" type="text"/>
+                        <input class="form-control" id="sku" name="sku" type="text" />
                     </div>
                     <div class="form-group">
                         <label class="control-label requiredField" for="name">
@@ -28,59 +42,61 @@
                                 *
                             </span>
                         </label>
-                        <input class="form-control" id="name" name="name" type="text"/>
+                        <input class="form-control" id="name" name="name" type="text" />
                     </div>
                     <div class="form-group">
                         <label class="control-label " for="price">
                             Price
                         </label>
-                        <input class="form-control" id="price" name="price" type="text"/>
+                        <input class="form-control" id="price" name="price" type="text" />
                     </div>
                     <div class="form-group">
                         <label class="control-label " for="qty">
                             Quantity
                         </label>
-                        <input class="form-control" id="qty" name="qty" type="text"/>
+                        <input class="form-control" id="qty" name="qty" type="text" />
                     </div>
                     <div class="form-group">
                         <label class="control-label " for="attribute-set">
                             Attribute Name
                         </label>
-                        <input class="form-control" id="attribute-set" name="attribute-set" type="text"/>
+                        <input class="form-control" id="attribute-set" name="attribute-set" type="text" />
                     </div>
-                    
+                    <label class="control-label " for="qty">
+                            Attribute Product
+                        </label>
                     <div class="panel panel-default">
-  <div class="panel-body">
-  
-  <div id="education_fields">
-          
-        </div>
-       <div class="col-sm-5 nopadding">
-  <div class="form-group">
-    <input type="text" class="form-control" id="Schoolname" name="Schoolname[]" value="" placeholder="School name">
-  </div>
-</div>
-<div class="col-sm-5 nopadding">
-  <div class="form-group">
-    <input type="text" class="form-control" id="Major" name="Major[]" value="" placeholder="Major">
-  </div>
-</div>
+                        <div class="panel-body">
 
-<div class="col-sm-2 nopadding">
-  <div class="form-group">
-    <div class="input-group">
- 
-      <div class="input-group-btn">
-        <button class="btn btn-success" type="button"  onclick="education_fields();"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> </button>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="clear"></div>
-  
-  </div>
-</div>
-<div class="form-group">
+                            <div id="education_fields">
+
+                            </div>
+                            <div class="col-sm-5 nopadding">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="attribute" name="attribute" value="" placeholder="Attribute">
+                                </div>
+                            </div>
+                            <div class="col-sm-5 nopadding">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="atrvalue" name="atrvalue" value="" placeholder="Value">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-2 nopadding">
+                                <div class="form-group">
+                                    <div class="input-group">
+
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-success" type="button" onclick="education_fields();"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+
+                        </div>
+                    </div>
+                    <div class="form-group">
                         <div>
                             <button class="btn btn-primary " name="submit" type="submit">
                                 Submit
@@ -94,23 +110,23 @@
 </div>
 
 <script type="text/javascript">
+    var room = 1;
 
-var room = 1;
-function education_fields() {
- 
-    room++;
-    var objTo = document.getElementById('education_fields')
-    var divtest = document.createElement("div");
-    divtest.setAttribute("class", "form-group removeclass"+room);
-    var rdiv = 'removeclass'+room;
-    divtest.innerHTML = `<div class="col-sm-5 nopadding">
+    function education_fields() {
+
+        room++;
+        var objTo = document.getElementById('education_fields')
+        var divtest = document.createElement("div");
+        divtest.setAttribute("class", "form-group removeclass" + room);
+        var rdiv = 'removeclass' + room;
+        divtest.innerHTML = `<div class="col-sm-5 nopadding">
     <div class="form-group">
-        <input type="text" class="form-control" id="Schoolname" name="Schoolname[]" value="" placeholder="School name">
+        <input type="text" class="form-control" id="attribute" name="attribute" value="" placeholder="Attribute">
     </div>
 </div>
 <div class="col-sm-5 nopadding">
     <div class="form-group">
-        <input type="text" class="form-control" id="Major" name="Major[]" value="" placeholder="Major">
+        <input type="text" class="form-control" id="atrvalue" name="atrvalue" value="" placeholder="Value">
     </div>
 </div>
 <div class="col-sm-2 nopadding">
@@ -123,10 +139,11 @@ function education_fields() {
     </div>
 </div>
 <div class="clear"></div>`;
-    
-    objTo.appendChild(divtest)
-}
-   function remove_education_fields(rid) {
-       $('.removeclass'+rid).remove();
-   }
+
+        objTo.appendChild(divtest)
+    }
+
+    function remove_education_fields(rid) {
+        $('.removeclass' + rid).remove();
+    }
 </script>
